2c6e6de06bf49d582dbe14a17d58b12f7b3aae10
saunaik19 <saunaik19@gmail.com>
2021-07-03 14:26:11 +0530

    userinfo object

--- src/main/java/com/example/completablefuture/dto/UserInfo.java
+++ src/main/java/com/example/completablefuture/dto/UserInfo.java
@@ -4,6 +4,7 @@ package com.example.completablefuture.dto;
 import lombok.*;
 
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -18,4 +19,22 @@ public class UserInfo {
     private String username;
     private String email;
     private List<Post> posts;
+    private List<Todo> todos;
+    
+    public UserInfo(String userId,UserInfo userObj,List<Post> posts){
+        this.setId(userObj.id);
+        this.setName(userObj.getName());
+        this.setUsername(userObj.getUsername());
+        this.setEmail(userObj.getEmail());
+        this.setPosts(posts);
+    }
+    public UserInfo(String userId,UserInfo userObj,List<Post> posts,List<Todo> toDos){
+        this.setId(userObj.id);
+        this.setName(userObj.getName());
+        this.setUsername(userObj.getUsername());
+        this.setEmail(userObj.getEmail());
+        this.setPosts(posts);
+        this.setTodos(toDos);
+    }
+
 }
